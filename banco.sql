6d03bcfce4f35137aab5cc8d61fdaeb3dbd1ac07
LUCAS DA MATA PRADO <lucas.mp7@puccampinas.edu.br>
2026-05-25 09:37:51 -0300

    Update banco.sql

--- banco.sql
+++ banco.sql
@@ -1,1 +1,10 @@
-
+CREATE DATABASE Aulas_particulares;
+USE Aulas_particulares;
+--- TABELAS ---
+CREATE TABLE alunos;
+CREATE TABLE professores;
+CREATE TABLE agendamentos;
+CREATE TABLE matérias;
+CREATE TABLE horários;
+CREATE TABLE logs;
+CREATE TABLE pagamentos;
