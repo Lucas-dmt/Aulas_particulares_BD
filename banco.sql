2baad747c416a6d403327d5759d8efc57d30b8ce
LUCAS DA MATA PRADO <lucas.mp7@puccampinas.edu.br>
2026-05-29 16:43:10 -0300

    adição dos comandos de visualização de dados

--- banco.sql
+++ banco.sql
@@ -9,14 +9,20 @@ CREATE TABLE alunos(
     sobrenome VARCHAR(120) NOT NULL,
     nome_social VARCHAR(120),
     horario_matricula DATETIME NOT NULL,
-    termos_concordados BOOLEAN NOT NULL DEFAULT FALSE);
+    termos_concordados BOOLEAN NOT NULL DEFAULT FALSE ,
+    termos_lgpd BOOLEAN DEFAULT FALSE , 
+    data_lgpd DATETIME
+    );
     
 CREATE TABLE professores(
 	id_professor INT PRIMARY KEY AUTO_INCREMENT,
     login VARCHAR(120) NOT NULL,
     senha VARCHAR(12) NOT NULL,
     nome VARCHAR(120) NOT NULL,
-    sobrenome VARCHAR(120) NOT NULL);
+    sobrenome VARCHAR(120) NOT NULL , 
+    termos_lgpd BOOLEAN DEFAULT FALSE , 
+    data_lgpd DATETIME
+    );
     
 CREATE TABLE materias(
 	id_materia INT PRIMARY KEY AUTO_INCREMENT,
@@ -51,32 +57,37 @@ CREATE TABLE logs(
     tabela_afetada VARCHAR(50) ,
     operacao VARCHAR(20) ,
     descricao TEXT , 
-    usuario_tipo VARCHAR(100) , --- aluno ou professor---
+    usuario_tipo VARCHAR(100) ,
     id_usuario INT , 
     data_hora DATETIME ,
     id_registro INT
 );
 --- INSERIR DADOS ---
-INSERT INTO alunos (login, senha, nome, sobrenome, nome_social, horario_matricula, termos_concordados) VALUES 
-("joice123", "123456", "Joice","Ariane","", "2026-05-28 10:05:00",TRUE),
-("Lucas1511", "151107", "Lucas", "Da mata","", "2026-05-30 09:44:00", TRUE);
+INSERT INTO alunos (login, senha, nome, sobrenome, nome_social, horario_matricula, termos_concordados, termos_lgpd, data_lgpd) VALUES 
+("joice123", "123456", "Joice","Ariane","", "2026-05-28 10:05:00",TRUE, TRUE, "2026-05-28 10:05:00"),
+("Lucas1511", "151107", "Lucas", "Da mata","", "2026-05-30 09:44:00", TRUE, TRUE, "2026-05-30 09:44:00"),
+("Victor3567", "3567", "Victor", "Marucci", "", "2026-05-30 18:00:00", TRUE, TRUE, "2026-05-30 18:00:00"),
+("Eduardo2525", "2525", "Eduardo", "Monteiro", "", "2026-06-15 15:15:00", TRUE, TRUE, "2026-06-15 15:15:00");
 
-INSERT INTO professores (login, senha, nome, sobrenome) VALUES
-("Alexandre453", "45316", "Alexandre", "Monteiro"),
-("Lucia1789", "1789", "Lúcia","Filomena");
+INSERT INTO professores (login, senha, nome, sobrenome, termos_lgpd, data_lgpd) VALUES
+("Alexandre453", "45316", "Alexandre", "Monteiro", TRUE, "2026-05-28 09:00:00"),
+("Lucia1789", "1789", "Lúcia","Filomena", TRUE, "2026-05-28 09:10:00");
 
 INSERT INTO materias (id_professor, nome_materia, preco_materia) VALUES
 (1, "Algébra linear", 50.00),
 (2, "Algoritmos de programação", 60.00);
 
 INSERT INTO horarios(dia_horario, status, id_materia, id_professor, id_aluno) VALUES 
-("2026-06-15", "OCUPADO", 1, 1, 1),
-("2026-06-17", "OCUPADO", 2,2,2);
+("2026-06-15", "OCUPADO", 1, 1, 11),
+("2026-06-17", "OCUPADO", 2,2,12),
+("2026-07-01", "OCUPADO", 2,2,13),
+("2026-07-14", "OCUPADO", 1,1,14);
 
 INSERT INTO pagamentos(id_materia, id_aluno, metodo_pagamento, status, data_expiracao) VALUES
-(1, 1, "PIX", TRUE, "2026-08-20"),
-(1, 2, "Cartao", TRUE, "2026-08-18");
-
+(1, 11, "PIX", TRUE, "2026-08-20"),
+(2, 12, "Cartao", TRUE, "2026-08-18"),
+(1, 13, "Cartao", TRUE, "2026-09-12"),
+(2, 14, "PIX", TRUE, "2026-09-25");
 INSERT INTO logs(tabela_afetada, operacao, descricao, usuario_tipo, data_hora, id_registro) VALUES
 ("alunos", "INSERT", "Cadastro de novo aluno", "aluno", "2026-05-28 10:00:00", 1), 
 ("alunos", "INSERT", "Cadastro de novo aluno", "aluno", "2026-05-28 10:50:00", 2), 
@@ -85,10 +96,39 @@ INSERT INTO logs(tabela_afetada, operacao, descricao, usuario_tipo, data_hora, i
 ("horarios", "DELETE", "Aula cancelada pelo aluno", "aluno", "2026-06-02 15:00:00", 2),
 ("horarios", "INSERT", "Aula agendada pelo aluno", "aluno", "2026-06-01 14:00:00", 1);
 
+--- VISUALIZAR DADOS ---
+SELECT a.nome, a.sobrenome, h.dia_horario, h.status
+FROM alunos a
+JOIN horarios h ON a.id_aluno = h.id_aluno;
 
+SELECT p.nome, p.sobrenome, m.nome_materia, m.preco_materia
+FROM professores p
+JOIN materias m ON p.id_professor = m.id_professor;
 
+SELECT * FROM logs
+ORDER BY data_hora DESC;
 
+SELECT COUNT(*) AS total_pagamentos
+FROM pagamentos
+WHERE status = TRUE;
 
+SELECT 
+    a.nome,
+    m.nome_materia,
+    pg.metodo_pagamento,
+    pg.status,
+    pg.data_expiracao
+FROM pagamentos pg
+LEFT JOIN alunos a ON pg.id_aluno = a.id_aluno
+LEFT JOIN materias m ON pg.id_materia = m.id_materia;
 
-
-
+SELECT 
+    h.dia_horario,
+    a.nome AS aluno,
+    p.nome AS professor,
+    m.nome_materia,
+    h.status
+FROM horarios h
+JOIN alunos a ON h.id_aluno = a.id_aluno
+JOIN professores p ON h.id_professor = p.id_professor
+JOIN materias m ON h.id_materia = m.id_materia;
